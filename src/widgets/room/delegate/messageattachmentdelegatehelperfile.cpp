46e1eeb6597f8d4bd8b404492a75627b6f6d3335
TheAssassin <theassassin@assassinate-you.net>
2021-08-04 00:48:07 +0200

    Restore ability to build on Ubuntu 20.04

--- src/widgets/room/delegate/messageattachmentdelegatehelperfile.cpp
+++ src/widgets/room/delegate/messageattachmentdelegatehelperfile.cpp
@@ -29,9 +29,11 @@
 #include "ruqolautils.h"
 #include "ruqolawidgets_debug.h"
 #include <KApplicationTrader>
-#include <KIO/ApplicationLauncherJob>
+//#include <KIO/ApplicationLauncherJob>
 #include <KIO/JobUiDelegate>
 #include <KLocalizedString>
+#include <KMimeTypeTrader>
+#include <KRun>
 #include <KService>
 
 #include <QMessageBox>
@@ -150,11 +152,16 @@ static void runApplication(const KService::Ptr &offer, const QString &link, QWid
     const QUrl downloadUrl = rcAccount->urlForLink(link);
     auto *job = rcAccount->restApi()->downloadFile(downloadUrl, fileUrl, QStringLiteral("text/plain"));
     QObject::connect(job, &RocketChatRestApi::DownloadFileJob::downloadFileDone, widget, [=](const QUrl &, const QUrl &localFileUrl) {
+#if KIO_VERSION >= QT_VERSION_CHECK(5, 69, 0)
         auto job = new KIO::ApplicationLauncherJob(offer); // asks the user if offer is nullptr
         job->setUrls({localFileUrl});
         job->setRunFlags(KIO::ApplicationLauncherJob::DeleteTemporaryFiles);
         job->setUiDelegate(new KIO::JobUiDelegate(KJobUiDelegate::AutoHandlingEnabled, widget));
         job->start();
+#else
+            KRun::runService(*offer, {localFileUrl}, widget, true);
+#endif
+
     });
 }
 
@@ -165,8 +172,13 @@ void MessageAttachmentDelegateHelperFile::handleDownloadClicked(const QString &l
     const QMimeType mimeType = db.mimeTypeForUrl(url);
     const bool valid = mimeType.isValid() && !mimeType.isDefault();
     const KService::Ptr offer = valid ?
+#if KIO_VERSION >= QT_VERSION_CHECK(5, 69, 0)
                                       KApplicationTrader::preferredService(mimeType.name())
                                       :
+#else
+                                      KMimeTypeTrader::self()->preferredService(mimeType.name())
+                                      :
+#endif
                                       KService::Ptr{};
     const UserChoice choice = askUser(url, offer, widget);
     switch (choice) {
